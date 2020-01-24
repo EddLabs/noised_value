e142396e9cdbade151a6f71b6b57463e1247b97b
Sagi Shadur <saroad2@gmail.com>
2020-01-24 16:59:48 +0200

    imlement lab_value

---
+++ src/lab_value/lab_value.py
@@ -0,0 +1,83 @@
+import math
+
+PLUS_MINUS = "\u00B1"
+
+
+class LabValue:
+
+    def __init__(self, val, var=None, err=None):
+        self.val = val
+        self.__set_initial_errors(var, err)
+
+    @property
+    def var(self):
+        if self.__var is None:
+            self.__set_errors()
+        return self.__var
+
+    @property
+    def err(self):
+        if self.__err is None:
+            self.__set_errors()
+        return self.__err
+
+    def __neg__(self):
+        return LabValue(val=-self.val, var=self.var)
+
+    def __add__(self, other):
+        if isinstance(other, LabValue):
+            return LabValue(val=self.val + other.val, var=self.var + other.var)
+        return LabValue(val=self.val + other, var=self.var)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):
+        return -(self + (-other))
+
+    def __mul__(self, other):
+        if isinstance(other, LabValue):
+            return LabValue(val=self.val * other.val, var=self.var * other.val ** 2 + other.var * self.val ** 2)
+        return LabValue(val=self.val * other, err=math.fabs(other) * self.err)
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        if isinstance(other, LabValue):
+            new_var = (self.var * other.val ** 2 + other.var * self.val ** 2) / (other.val ** 4)
+            return LabValue(val=self.val / other.val, var=new_var)
+        return LabValue(val=self.val / other, err=self.err / math.fabs(other))
+
+    def __rtruediv__(self, other):
+        return LabValue(val=other) / self
+
+    def __pow__(self, power):
+        return LabValue(val=self.val ** power, err=power * self.err * self.val ** (power - 1))
+
+    def __set_initial_errors(self, var, err):
+        if var is None and err is None:
+            self.__var = self.__err = 0
+            return
+        if var is not None:
+            if var < 0:
+                raise ValueError(f"Variance must be non-negative, got {var}")
+            if err is not None:
+                raise ValueError("Cannot create a LabValue with both variance and error")
+        if err is not None and err < 0:
+            raise ValueError(f"Error must be non-negative, got {err}")
+        self.__var = var
+        self.__err = err
+
+    def __set_errors(self):
+        if self.__var is not None:
+            self.__err = math.sqrt(self.__var)
+        elif self.__err is not None:
+            self.__var = self.__err ** 2
+
+    def __repr__(self):
+        return f"{self.val} {PLUS_MINUS} {self.err}"
+
---
+++ src/test/test_simple_lab_value_calculations.py
@@ -0,0 +1,241 @@
+from unittest import TestCase
+from lab_value.lab_value import LabValue
+
+
+class TestLabValue(TestCase):
+
+    def setUp(self):
+        self.places = 5
+
+    def check(self):
+        self.assertAlmostEqual(self.expected_val, self.v.val, places=self.places,
+                               msg="Value is different than expected")
+        self.assertAlmostEqual(self.expected_var, self.v.var, places=self.places,
+                               msg="Variance is different than expected")
+        self.assertAlmostEqual(self.expected_err, self.v.err, places=self.places,
+                               msg="Error is different than expected")
+
+    # Initialization
+
+    def test_init_without_variance_or_error(self):
+        self.v = LabValue(val=1)
+        self.expected_val = 1
+        self.expected_var = 0
+        self.expected_err = 0
+
+        self.check()
+
+    def test_init_with_variance(self):
+        self.v = LabValue(val=1, var=0.5)
+        self.expected_val = 1
+        self.expected_var = 0.5
+        self.expected_err = 0.70711
+
+        self.check()
+
+    def test_init_with_error(self):
+        self.v = LabValue(val=1, err=0.5)
+        self.expected_val = 1
+        self.expected_var = 0.25
+        self.expected_err = 0.5
+
+        self.check()
+
+    def test_init_with_both_variance_and_error_raises_value_error(self):
+        self.assertRaises(ValueError, LabValue, val=1, var=0.25, err=0.5)
+
+    def test_init_with_negative_variance_raises_value_error(self):
+        self.assertRaises(ValueError, LabValue, val=1, var=-0.25)
+
+    def test_init_with_negative_error_raises_value_error(self):
+        self.assertRaises(ValueError, LabValue, val=1, err=-0.5)
+
+    # Representation
+
+    def test_representation(self):
+        v = LabValue(val=1, err=0.5)
+        self.assertEqual("1 \u00B1 0.5", str(v), msg="LabUtil representation is different than expected")
+
+    # Negative
+
+    def test_negative(self):
+        self.v = -LabValue(val=1, var=0.5)
+        self.expected_val = -1
+        self.expected_var = 0.5
+        self.expected_err = 0.70711
+
+        self.check()
+
+    # Add
+
+    def test_right_add_constant(self):
+        self.v = LabValue(val=1, var=0.5) + 2.2
+        self.expected_val = 3.2
+        self.expected_var = 0.5
+        self.expected_err = 0.70711
+
+        self.check()
+
+    def test_left_add_constant(self):
+        self.v = 2.2 + LabValue(val=1, var=0.5)
+        self.expected_val = 3.2
+        self.expected_var = 0.5
+        self.expected_err = 0.70711
+
+        self.check()
+
+    def test_add_other_lab_value(self):
+        self.v = LabValue(val=1, var=0.5) + LabValue(val=2.2, var=2.75)
+        self.expected_val = 3.2
+        self.expected_var = 3.25
+        self.expected_err = 1.802775
+
+        self.check()
+
+    # Subtract
+
+    def test_right_subtract_constant(self):
+        self.v = LabValue(val=1, var=0.5) - 2.2
+        self.expected_val = -1.2
+        self.expected_var = 0.5
+        self.expected_err = 0.70711
+
+        self.check()
+
+    def test_left_subtract_constant(self):
+        self.v = 2.2 - LabValue(val=1, var=0.5)
+        self.expected_val = 1.2
+        self.expected_var = 0.5
+        self.expected_err = 0.70711
+
+        self.check()
+
+    def test_subtract_other_lab_value(self):
+        self.v = LabValue(val=1, var=0.5) - LabValue(val=2.2, var=2.75)
+        self.expected_val = -1.2
+        self.expected_var = 3.25
+        self.expected_err = 1.802775
+
+        self.check()
+
+    # Multiply
+
+    def test_right_multiply_positive_constant(self):
+        self.v = LabValue(val=1.5, var=0.5) * 1.12
+        self.expected_val = 1.68
+        self.expected_var = 0.6272
+        self.expected_err = 0.79196
+
+        self.check()
+
+    def test_right_multiply_negative_constant(self):
+        self.v = LabValue(val=1.5, var=0.5) * (-1.12)
+        self.expected_val = -1.68
+        self.expected_var = 0.6272
+        self.expected_err = 0.79196
+
+        self.check()
+
+    def test_left_multiply_positive_constant(self):
+        self.v = 1.12 * LabValue(val=1.5, var=0.5)
+        self.expected_val = 1.68
+        self.expected_var = 0.6272
+        self.expected_err = 0.79196
+
+        self.check()
+
+    def test_left_multiply_negative_constant(self):
+        self.v = -1.12 * LabValue(val=1.5, var=0.5)
+        self.expected_val = -1.68
+        self.expected_var = 0.6272
+        self.expected_err = 0.79196
+
+        self.check()
+
+    def test_multiply_other_positive_lab_value(self):
+        self.v = LabValue(val=1.5, var=0.5) * LabValue(val=2.3, var=2.75)
+        self.expected_val = 3.45
+        self.expected_var = 8.8325
+        self.expected_err = 2.97195
+
+        self.check()
+
+    def test_multiply_other_negative_lab_value(self):
+        self.v = LabValue(val=1.5, var=0.5) * LabValue(val=-2.3, var=2.75)
+        self.expected_val = -3.45
+        self.expected_var = 8.8325
+        self.expected_err = 2.97195
+
+        self.check()
+
+    # Division
+
+    def test_right_divide_positive_constant(self):
+        self.v = LabValue(val=1.5, var=0.5) / 1.12
+        self.expected_val = 1.339285
+        self.expected_var = 0.3986
+        self.expected_err = 0.631345
+
+        self.check()
+
+    def test_right_divide_negative_constant(self):
+        self.v = LabValue(val=1.5, var=0.5) / (-1.12)
+        self.expected_val = -1.339285
+        self.expected_var = 0.3986
+        self.expected_err = 0.631345
+
+        self.check()
+
+    def test_left_divide_positive_constant(self):
+        self.v = 1.12 / LabValue(val=1.5, var=0.5)
+        self.expected_val = 0.746666
+        self.expected_var = 0.12389
+        self.expected_err = 0.35198
+
+        self.check()
+
+    def test_divide_other_lab_value(self):
+        self.v = LabValue(val=1.5, var=0.5) / LabValue(val=1.12, var=0.2)
+        self.expected_val = 1.339285
+        self.expected_var = 0.68458
+        self.expected_err = 0.82739
+
+        self.check()
+
+    # Power
+
+    def test_1st_power(self):
+        self.v = LabValue(val=1.5, err=0.5) ** 1
+        self.expected_val = 1.5
+        self.expected_var = 0.25
+        self.expected_err = 0.5
+
+        self.check()
+
+    def test_2nd_power(self):
+        self.v = LabValue(val=1.5, err=0.5) ** 2
+        self.expected_val = 2.25
+        self.expected_var = 2.25
+        self.expected_err = 1.5
+
+        self.check()
+
+    def test_3rd_power(self):
+        self.v = LabValue(val=1.5, err=0.5) ** 3
+        self.expected_val = 3.375
+        self.expected_var = 11.390625
+        self.expected_err = 3.375
+
+        self.check()
+
+    def test_0_power(self):
+        self.v = LabValue(val=1.5, err=0.5) ** 0
+        self.expected_val = 1
+        self.expected_var = 0
+        self.expected_err = 0
+
+        self.check()
+
+    def test_negative_power_raise_value_error(self):
+        v = LabValue(val=1.5, err=0.5)
+        self.assertRaises(ValueError, v.__pow__, -1)
